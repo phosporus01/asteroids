9b5e2bd94b62a141bd3148f359b1f2b8c5dd9e38
Josonpop <jos@popeye.home>
2025-03-06 19:40:18 +0100

    used groups instead pilling on in the game loop

--- main.py
+++ main.py
@@ -10,19 +10,28 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     clock = pygame.time.Clock() 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2) 
+    
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         
-        player.update(dt)
+        updatable.update(dt)
         
         screen.fill("black")
-        player.draw(screen)
+        for obj in drawable:
+            obj.draw(screen)
+        
         pygame.display.flip()
+        
         # Framerate limit 60fps 
         dt = clock.tick(60) / 1000
 
