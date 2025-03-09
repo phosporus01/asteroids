d3f08c79ae47048007c4e237cf020a2223e495ab
Josonpop <jos@popeye.home>
2025-03-09 15:21:44 +0100

    brrrrt, the spaceship shoots now

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from constants import *
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+from shot import Shot
 
 def main():
     print("Starting Asteroids!")
@@ -16,15 +17,15 @@ def main():
     clock = pygame.time.Clock() 
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
-
     asteroids = pygame.sprite.Group()
-
     asteroidfield = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+
 
     Player.containers = (updatable, drawable)
     Asteroid.containers = (asteroids, updatable, drawable)
     AsteroidField.containers = (updatable)
-
+    Shot.containers = (shots, updatable, drawable)
     asteroid_field = AsteroidField() 
 
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2) 
--- player.py
+++ player.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import *
 from circleshape import CircleShape
+from shot import Shot
 
 class Player(CircleShape):
     def __init__(self, x, y):
@@ -25,7 +26,9 @@ class Player(CircleShape):
 
         if keys[pygame.K_s]:
             self.move(-dt)
-    
+        
+        if keys[pygame.K_SPACE]:
+            self.shoot()
 
 
     def triangle(self):
@@ -43,3 +46,11 @@ class Player(CircleShape):
     def move(self, dt):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         self.position += forward * PLAYER_SPEED * dt
+
+    def shoot(self):
+        shot = Shot(self.position.x, self.position.y)
+        shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
+
+
+
+
